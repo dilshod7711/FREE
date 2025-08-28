d8fd5ec5d04219c9b5f3135a0ba8152ab240235b
MacBook Air <macbookair@MacBook-Air-MacBook.local>
2025-08-28 19:02:30 +0500

    add

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-// const img = document.getElementById("header")
+const user = {
+  username: "dilshod",
+  password: "123456",
+  setPassword(password) {
+    if (password === this.password) {
+      console.log(" bir xil");
+    } else {
+      console.log("bir xil emas");
+    }
+  },
+};
 
+user.setPassword("123456");
+
+// const img = document.getElementById("header")
 
 // img.addEventListener("click", () => {
 //     img.style.backgroundImage = "url('./img/header2.png')"
